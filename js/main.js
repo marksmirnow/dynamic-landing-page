6a0a3761e39c170cfa8f1f1cb34e65bf99505cc6
marksmirnow <mark.smirnow2@gmail.com>
2021-03-29 01:58:25 +0300

    feat: add function showTime and run it

--- js/main.js
+++ js/main.js
@@ -5,4 +5,15 @@ const greeting = document.getElementById('greeting');
 const name = document.getElementById('name');
 const focus = document.getElementById('focus');
 
-console.log(time, greeting, name, focus);
+function showTime() {
+	let today = new Date();
+	let hour = today.getHours();
+	let min = today.getMinutes();
+	let sec = today.getSeconds();
+
+	time.innerHTML = `${hour}:${min}:${sec}`;
+
+	setTimeout(showTime, 1000);
+}
+
+showTime();
