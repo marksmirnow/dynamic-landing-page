7655617fccea3abd83deb9eb7783d646f637237a
marksmirnow <mark.smirnow2@gmail.com>
2021-03-29 02:31:13 +0300

    feat: add function addZero and run it

--- js/main.js
+++ js/main.js
@@ -11,9 +11,15 @@ function showTime() {
 	let min = today.getMinutes();
 	let sec = today.getSeconds();
 
-	time.innerHTML = `${hour}:${min}:${sec}`;
+	time.innerHTML = `${hour}:${addZero(min)}:${addZero(sec)}`;
 
 	setTimeout(showTime, 1000);
 }
+
+
+function addZero(n) {
+	return ((n < 10) ? '0' : '') + n;
+}
+
 
 showTime();
