c19adc5a0eb8db93995cfa35dda1756b1ad118f0
marksmirnow <mark.smirnow2@gmail.com>
2021-03-29 01:09:10 +0300

    get elements of page

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Get elements
+
+const time = document.getElementById('time');
+const greeting = document.getElementById('greeting');
+const name = document.getElementById('name');
+const focus = document.getElementById('focus');
+
+console.log(time, greeting, name, focus);
